723c3adc333d0ee8b7e419ff9873d46b005f7930
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-12 14:11:57 -0400

    added nav bar and fixed scroll

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-zinc-900 " }>{children}</body>
+      <body className={inter.className + " bg-zinc-900"}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
